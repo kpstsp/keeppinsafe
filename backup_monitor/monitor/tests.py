7e8a33909d62a2a81f7ae9c7f0372da9fb72b4bb
trx <Konstantin Parashchevin>
2024-11-07 16:43:06 +0400

    Simple unit test for monitor view

--- backup_monitor/monitor/tests.py
+++ backup_monitor/monitor/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+# backup_monitor/monitor/tests.py
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class MonitorViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+
+    def test_calendar_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('calendar'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'monitor/calendar.html')
